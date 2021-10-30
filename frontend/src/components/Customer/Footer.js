cf00c098b0959ef2cff5b07cc16ec61abe0ae149
Benngu235 <nguyenanhquan2801@gmail.com>
2021-10-30 13:32:30 +0700

    fix ui customer's home page

--- frontend/src/components/Customer/Footer.js
+++ frontend/src/components/Customer/Footer.js
@@ -1,82 +1,93 @@
-import React from 'react';
-import  './Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import logo from "../../images/medcareicon.png";
+import phone from "../../images/phone.png";
+import mail from "../../images/mail.png";
+import location from "../../images/location.png";
 function Footer() {
   return (
-    <div className='footer-container'>
-      <div class='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+    <div className="footer-container">
+      <div class="footer-links">
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
             <h2>WHO WE ARE</h2>
-            <img className='footer-logo' src='/images/end.png' />
+            <img className="footer-logo" src={logo} />
             <h5>Making health care</h5>
             <h5>better together</h5>
           </div>
-          <div class='footer-link-items'>
+          <div class="footer-link-items">
             <h2>USEFUL LINKS</h2>
-            <Link to='/sign-up'>Home</Link>
-            <Link to='/'>About</Link>
-            <Link to='/'>Contract Us</Link>
+            <Link to="/sign-up">Home</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Contact Us</Link>
           </div>
-          <div class='footer-link-items'>
+          <div class="footer-link-items">
             <h2>GET IN TOUCH</h2>
-            <Link to='/'><img className='footer-icon' src='/images/phone.png' /> 0931 910 JQK</Link>
-            <Link to='/'><img className='footer-icon' src='/images/mail.png' /> medcare@med.care</Link>
-            <Link to='/'><img className='footer-icon' src='/images/location.png' /> 18 Hoang Quoc Viet</Link>
+            <Link to="/">
+              <img className="footer-icon" src={phone} /> 0931 910 JQK
+            </Link>
+            <Link to="/">
+              <img className="footer-icon" src={mail} /> medcare@med.care
+            </Link>
+            <Link to="/">
+              <img className="footer-icon" src={location} /> 18 Hoang Quoc Viet
+            </Link>
           </div>
-          <div class='footer-link-items'>
+          <div class="footer-link-items">
             <h2>LANGUAGE</h2>
-            <Link to='/'>English</Link>
-            <Link to='/'>Tiếng Việt</Link>
-            <Link to='/'>Francais</Link>
+            <Link to="/">English</Link>
+            <Link to="/">Tiếng Việt</Link>
+            <Link to="/">Francais</Link>
           </div>
         </div>
       </div>
-      <section class='social-media'>
+      <section class="social-media">
         <div>
-          <small class='website-rights'>Copyright © 2021 Medcare. All rights reserved.</small>
-          <div class='social-icons'>
+          <small class="website-rights">
+            Copyright © 2021 Medcare. All rights reserved.
+          </small>
+          <div class="social-icons">
             <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
+              class="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
             >
-              <i class='fab fa-facebook-f' />
+              <i class="fab fa-facebook-f" />
             </Link>
             <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
+              class="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
             >
-              <i class='fab fa-instagram' />
+              <i class="fab fa-instagram" />
             </Link>
             <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
+              class="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
             >
-              <i class='fab fa-youtube' />
+              <i class="fab fa-youtube" />
             </Link>
             <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
             >
-              <i class='fab fa-twitter' />
+              <i class="fab fa-twitter" />
             </Link>
             <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
             >
-              <i class='fab fa-linkedin' />
+              <i class="fab fa-linkedin" />
             </Link>
           </div>
         </div>
